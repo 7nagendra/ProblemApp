b66428d9bfea1a0c6eb7f07007ca01ba7f735911
preddy <preddy@kiwiup.com>
2013-03-27 19:29:39 +0530

    include facebook, currently login button will directly take you to map, cause the FB app needs to be registered before we fix this

--- src/com/innoapps/meeting/ShowMap.java
+++ src/com/innoapps/meeting/ShowMap.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 //import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class ShowMap extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//setContentView(R.layout.activity_main);
-		setContentView(R.layout.main);
+		setContentView(R.layout.map);
 	}
 
 /*	@Override
